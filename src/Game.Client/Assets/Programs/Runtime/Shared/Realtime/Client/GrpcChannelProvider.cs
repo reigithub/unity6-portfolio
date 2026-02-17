5c010cdf7ea3cb5dfb02bb9ef3434e145aeca9ac
Rei.K <rei.engineer.hub@gmail.com>
2026-02-17 12:51:24 +0900

    パッケージ解決

--- src/Game.Client/Assets/Programs/Runtime/Shared/Realtime/Client/GrpcChannelProvider.cs
+++ src/Game.Client/Assets/Programs/Runtime/Shared/Realtime/Client/GrpcChannelProvider.cs
@@ -1,4 +1,5 @@
 using System;
+using Cysharp.Net.Http;
 using Game.Shared;
 using Grpc.Net.Client;
 using UnityEngine;
@@ -8,10 +9,12 @@ namespace Game.Shared.Realtime.Client
     /// <summary>
     /// gRPC チャンネル管理実装
     /// GrpcChannel のライフサイクルを管理し、Singleton として DI に登録
+    /// YetAnotherHttpHandler でネイティブ HTTP/2 を使用（StreamingHub 双方向ストリーミング対応）
     /// </summary>
     public class GrpcChannelProvider : IGrpcChannelProvider
     {
         private GrpcChannel _channel;
+        private YetAnotherHttpHandler _httpHandler;
         private bool _disposed;
 
         public bool IsConnected => _channel != null && !_disposed;
@@ -44,10 +47,12 @@ namespace Game.Shared.Realtime.Client
                 _disposed = true;
                 _channel?.Dispose();
                 _channel = null;
+                _httpHandler?.Dispose();
+                _httpHandler = null;
             }
         }
 
-        private static GrpcChannel CreateChannel()
+        private GrpcChannel CreateChannel()
         {
             var config = GameEnvironmentHelper.CurrentConfig;
             var grpcUrl = config?.GrpcBaseUrl;
@@ -61,9 +66,12 @@ namespace Game.Shared.Realtime.Client
 
             Debug.Log("[GrpcChannelProvider] Connecting to gRPC server: " + grpcUrl);
 
+            _httpHandler?.Dispose();
+            _httpHandler = new YetAnotherHttpHandler { Http2Only = true };
+
             return GrpcChannel.ForAddress(grpcUrl, new GrpcChannelOptions
             {
-                HttpHandler = new Grpc.Net.Client.Web.GrpcWebHandler(new System.Net.Http.HttpClientHandler()),
+                HttpHandler = _httpHandler,
             });
         }
     }
