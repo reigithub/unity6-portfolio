83300434440c090f013ee55aeccc2bdbd9d082ae
Rei.K <rei.engineer.hub@gmail.com>
2026-04-12 15:55:29 +0900

    アドレス解決エラー

--- src/Game.Realtime/Services/UnityServerConfiguration.cs
+++ src/Game.Realtime/Services/UnityServerConfiguration.cs
@@ -5,6 +5,6 @@ namespace Game.Realtime.Services;
 /// </summary>
 public class UnityServerConfiguration
 {
-    public string ServerAddress { get; set; } = "localhost";
+    public string ServerAddress { get; set; } = "127.0.0.1";
     public int ServerPort { get; set; } = 7777;
 }
