1c2629337c8f4b4d384c230178a9e0ab15e04502
Rei.K <71711255+reigithub@users.noreply.github.com>
2026-05-13 01:40:46 +0900

    UnitySceneベース追加 (#204)

--- src/Game.Client/Assets/Programs/Runtime/MVP/Core/Constants/GameSceneConstants.cs
+++ src/Game.Client/Assets/Programs/Runtime/MVP/Core/Constants/GameSceneConstants.cs
@@ -4,6 +4,8 @@ namespace Game.MVP.Core.Constants
 {
     public static class GameSceneConstants
     {
+        public const string GameRootSceneName = "GameRootScene";
+
         public const GameSceneOperations DefaultOperations = GameSceneOperations.CurrentSceneTerminate |
                                                              GameSceneOperations.CurrentSceneClearHistory;
     }
