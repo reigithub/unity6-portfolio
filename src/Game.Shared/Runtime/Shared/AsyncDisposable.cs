7123a10d2a851b9c69df0145b24df6fcf8058244
Rei.K <rei.engineer.hub@gmail.com>
2026-05-25 19:06:33 +0900

    AsyncDisposable

---
+++ src/Game.Shared/Runtime/Shared/AsyncDisposable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Game.Library.Shared
+{
+    public sealed class AsyncDisposable : IAsyncDisposable
+    {
+        private readonly Func<ValueTask> _cleanup;
+
+        public AsyncDisposable(Func<ValueTask> cleanup)
+        {
+            _cleanup = cleanup;
+        }
+
+        public ValueTask DisposeAsync() => _cleanup();
+
+        public static IAsyncDisposable Create(Func<ValueTask> action) => new AsyncDisposable(action);
+    }
+    public sealed class EmptyAsyncDisposable : IAsyncDisposable
+    {
+        public ValueTask DisposeAsync() => new();
+    }
+}
