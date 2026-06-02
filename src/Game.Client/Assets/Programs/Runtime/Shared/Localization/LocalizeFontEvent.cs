b19fbb70a56b0b945a8d9ed2f7bf71f2d9d63fea
Rei.K <rei.engineer.hub@gmail.com>
2026-06-02 14:29:01 +0900

    ローカライズ用UIコンポーネント

---
+++ src/Game.Client/Assets/Programs/Runtime/Shared/Localization/LocalizeFontEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Components;
+
+namespace Game.Shared.Localization
+{
+    [AddComponentMenu("Localization/Asset/Localize TmpFont Event")]
+    public class LocalizeTmpFontEvent : LocalizedAssetEvent<TMP_FontAsset, LocalizedTmpFont, UnityEventTmpFont>
+    {
+    }
+
+    [Serializable]
+    public class UnityEventTmpFont : UnityEvent<TMP_FontAsset>
+    {
+    }
+}
