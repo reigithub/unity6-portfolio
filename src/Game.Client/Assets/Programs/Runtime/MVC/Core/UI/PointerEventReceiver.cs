5d452736c00b2c4b6fa23a53d5286a9be87aa2d3
Rei.K <rei.engineer.hub@gmail.com>
2026-06-06 00:18:58 +0900

    PointerEventReceiverにOnPointerClick追加

--- src/Game.Client/Assets/Programs/Runtime/MVC/Core/UI/PointerEventReceiver.cs
+++ src/Game.Client/Assets/Programs/Runtime/MVC/Core/UI/PointerEventReceiver.cs
@@ -1,22 +1,35 @@
 using Game.Core.Services;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace Game.Core.UI
 {
-    public class PointerEventReceiver : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    public class PointerEventReceiver : MonoBehaviour
+        , IPointerEnterHandler, IPointerExitHandler
+        , IPointerClickHandler
     {
+        [SerializeField] private Selectable _selectable;
+
         private InputSystemService _inputService;
         private InputSystemService InputService => _inputService ??= GameServiceManager.Get<InputSystemService>();
 
-        public void OnPointerEnter(PointerEventData eventData)
-        {
-            InputService.SetSelectedGameObject(gameObject);
-        }
+        public void OnPointerEnter(PointerEventData eventData) => OnSelected();
+        public void OnPointerExit(PointerEventData eventData) => OnDeselected();
+        public void OnPointerClick(PointerEventData eventData) => OnSelected();
 
-        public void OnPointerExit(PointerEventData eventData)
+        private void OnSelected() => InputService.SetSelectedGameObject(_selectable.gameObject);
+        private void OnDeselected() => InputService.SetSelectedGameObject(null);
+
+#if UNITY_EDITOR
+        private void OnValidate()
         {
-            InputService.SetSelectedGameObject(null);
+            if (_selectable != null)
+                return;
+
+            if (gameObject.TryGetComponent<Selectable>(out var selectable))
+                _selectable = selectable;
         }
+#endif
     }
 }
