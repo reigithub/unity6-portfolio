14364aeaef14a74ff3a2ac053f0ed8d4aaca58ab
Rei.K <rei.engineer.hub@gmail.com>
2026-06-05 16:17:45 +0900

    GenericValues値インデクサ

--- src/Game.Client/Assets/Programs/Runtime/MVC/Core/UI/GenericValues.cs
+++ src/Game.Client/Assets/Programs/Runtime/MVC/Core/UI/GenericValues.cs
@@ -1,8 +1,10 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Game.Core.UI
 {
-    [System.Serializable]
+    [Serializable]
     public class GenericValues<T>
     {
         [SerializeField] private T[] _values;
@@ -10,7 +12,12 @@ namespace Game.Core.UI
         public T this[int index]
         {
             get => _values[index];
-            set => _values[index] = value;
+            // set => _values[index] = value;
+        }
+
+        public int this[T val]
+        {
+            get => Math.Max(0, Array.FindIndex(_values, x => EqualityComparer<T>.Default.Equals(x, val)));
         }
 
         public int Count => _values.Length;
