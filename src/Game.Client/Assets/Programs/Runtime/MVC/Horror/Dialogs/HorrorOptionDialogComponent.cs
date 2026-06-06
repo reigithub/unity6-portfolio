c972501cc767f2454dff65780937c64371df74f6
Rei.K <rei.engineer.hub@gmail.com>
2026-06-06 23:37:21 +0900

    オプション画面実装(仮)

--- src/Game.Client/Assets/Programs/Runtime/MVC/Horror/Dialogs/HorrorOptionDialogComponent.cs
+++ src/Game.Client/Assets/Programs/Runtime/MVC/Horror/Dialogs/HorrorOptionDialogComponent.cs
@@ -1,3 +1,4 @@
+using System;
 using Cysharp.Threading.Tasks;
 using Game.Core.Services;
 using Game.Core.UI;
@@ -16,6 +17,8 @@ namespace Game.Horror.Dialogs
     {
         protected override string AssetPathOrAddress => "HorrorOptionDialog";
 
+        private HorrorOptionDialogModel _model;
+
         private InputSystemService _inputService;
         private InputSystemService InputService => _inputService ??= GameServiceManager.Get<InputSystemService>();
 
@@ -23,6 +26,12 @@ namespace Game.Horror.Dialogs
         {
             var sceneService = GameServiceManager.Get<GameSceneService>();
             return await sceneService.TransitionDialogAsync<HorrorOptionDialog, bool>();
+        }
+
+        public override UniTask PreInitialize()
+        {
+            _model = new HorrorOptionDialogModel();
+            return base.PreInitialize();
         }
 
         public override UniTask Startup()
@@ -43,6 +52,21 @@ namespace Game.Horror.Dialogs
                 .Where(_ => State.IsProcessing())
                 .Subscribe(_ => SceneComponent.NextTab())
                 .AddTo(Disposables);
+
+            SceneComponent.OnLanguageChanged
+                .Subscribe(code =>
+                {
+                    _model.SetLocalCode(code);
+                    var locale = LocalizationSettings.AvailableLocales.GetLocale(code);
+                    LocalizationSettings.SelectedLocale = locale;
+                })
+                .AddTo(Disposables);
+
+            // Audio
+            SceneComponent.OnMasterVolumeChanged.Subscribe(_model.SetMasterVolume).AddTo(Disposables);
+            SceneComponent.OnBgmVolumeChanged.Subscribe(_model.SetBgmVolume).AddTo(Disposables);
+            SceneComponent.OnVoiceVolumeChanged.Subscribe(_model.SetVoiceVolume).AddTo(Disposables);
+            SceneComponent.OnSeVolumeChanged.Subscribe(_model.SetSeVolume).AddTo(Disposables);
 
             return base.Startup();
         }
@@ -66,25 +90,80 @@ namespace Game.Horror.Dialogs
 
         [SerializeField] private SliderValueSelector _cameraAcceleration;
         [SerializeField] private SliderValueSelector _cameraShake;
+        [SerializeField] private SliderValueSelector _cameraFov;
 
-        [Header("Options - Video")]
+        [Header("Options - Display")]
         [SerializeField] private SliderIndexSelector _displayMode;
         [SerializeField] private GenericValues<FullScreenMode> _displayModeValues;
 
         [SerializeField] private SliderIndexSelector _resolution;
         [SerializeField] private GenericValues<ResolutionInfo> _resolutionValues;
 
-        [SerializeField] private SliderValueSelector _fov;
+        [SerializeField] private SliderIndexSelector _frameRateLimit;
+        [SerializeField] private GenericValues<FrameRateLimit> _frameRateLimitValues;
+
+        [SerializeField] private SliderIndexSelector _vSync;
+        [SerializeField] private SliderIndexSelector _motionBluer;
 
         [Header("Options - Graphics")]
-        [SerializeField] private SliderIndexSelector _graphicsPreset;
-        [SerializeField] private GenericValues<GraphicQuality> _graphicsPresetValues;
+        [SerializeField] private SliderIndexSelector _graphicQualityPreset;
+        [SerializeField] private GenericValues<GraphicQuality> _graphicQualityValues;
+
+        [SerializeField] private SliderValueSelector _resolutionScale;
+
+        [SerializeField] private SliderIndexSelector _lighting;
+        [SerializeField] private SliderIndexSelector _reflection;
+        [SerializeField] private SliderIndexSelector _antiAliasing;
+        [SerializeField] private SliderIndexSelector _postProcessing;
 
         [Header("Options - Audio")]
         [SerializeField] private SliderValueSelector _masterVolume;
         [SerializeField] private SliderValueSelector _bgmVolume;
         [SerializeField] private SliderValueSelector _voiceVolume;
         [SerializeField] private SliderValueSelector _seVolume;
+
+        #endregion
+
+        #region Options - General
+
+        public Observable<string> OnLanguageChanged => _language.OnValueChanged.Select(index => _languageValues[index]);
+
+        public Observable<bool> OnCameraControlHorizontalChanged => _cameraControlHorizontal.OnValueChanged.Select(index => index != 0);
+        public Observable<bool> OnCameraControlVerticalChanged => _cameraControlVertical.OnValueChanged.Select(index => index != 0);
+        public Observable<float> OnCameraSensitivityHorizontalChanged => _cameraSensitivityHorizontal.OnValueChanged;
+        public Observable<float> OnCameraSensitivityVerticalChanged => _cameraSensitivityVertical.OnValueChanged;
+        public Observable<float> OnCameraAccelerationChanged => _cameraAcceleration.OnValueChanged;
+        public Observable<float> OnCameraShakeChanged => _cameraShake.OnValueChanged;
+
+        #endregion
+
+        #region Options - Video
+
+        public Observable<FullScreenMode> OnDisplayModeChanged => _displayMode.OnValueChanged.Select(index => _displayModeValues[index]);
+        public Observable<ResolutionInfo> OnResolutionChanged => _resolution.OnValueChanged.Select(index => _resolutionValues[index]);
+        public Observable<float> OnFovChanged => _cameraFov.OnValueChanged;
+
+        #endregion
+
+        #region Options - Graphics
+
+        public Observable<GraphicQuality> OnGraphicQualityPresetChanged => _graphicQualityPreset.OnValueChanged.Select(index => _graphicQualityValues[index]);
+
+        public Observable<float> OnResolutionScaleChanged => _resolutionScale.OnValueChanged;
+
+        public Observable<GraphicQuality> OnLightingChanged => _lighting.OnValueChanged.Select(index => _graphicQualityValues[index]);
+        public Observable<GraphicQuality> OnReflectionChanged => _reflection.OnValueChanged.Select(index => _graphicQualityValues[index]);
+        public Observable<GraphicQuality> OnAntiAliasingChanged => _antiAliasing.OnValueChanged.Select(index => _graphicQualityValues[index]);
+        public Observable<GraphicQuality> OnPostProcessingChanged => _postProcessing.OnValueChanged.Select(index => _graphicQualityValues[index]);
+
+        #endregion
+
+        #region Audio
+
+        public Observable<float> OnMasterVolumeChanged => _masterVolume.OnValueChanged;
+        public Observable<float> OnBgmVolumeChanged => _bgmVolume.OnValueChanged;
+        public Observable<float> OnVoiceVolumeChanged => _voiceVolume.OnValueChanged;
+        public Observable<float> OnSeVolumeChanged => _seVolume.OnValueChanged;
 
         #endregion
 
@@ -110,14 +189,15 @@ namespace Game.Horror.Dialogs
             }
 #endif
 
-            _language.OnValueChanged
-                .Subscribe(index =>
-                {
-                    var code = _languageValues[index];
-                    var locale = LocalizationSettings.AvailableLocales.GetLocale(code);
-                    LocalizationSettings.SelectedLocale = locale;
-                })
-                .AddTo(Disposables);
+            // _language.OnValueChanged
+            //     .Subscribe(index =>
+            //     {
+            //         var code = _languageValues[index];
+            //         // var locale = LocalizationSettings.AvailableLocales.GetLocale(code);
+            //         // LocalizationSettings.SelectedLocale = locale;
+            //         // _onLanguageChanged.OnNext(code);
+            //     })
+            //     .AddTo(Disposables);
 
             #endregion
 
@@ -209,7 +289,7 @@ namespace Game.Horror.Dialogs
                 })
                 .AddTo(Disposables);
 
-            _fov.OnValueChanged
+            _cameraFov.OnValueChanged
                 .Subscribe(value =>
                 {
                     Debug.Log($"Option Fov: {value}");
@@ -220,10 +300,10 @@ namespace Game.Horror.Dialogs
 
             #region Graphics
 
-            _graphicsPreset.OnValueChanged
+            _graphicQualityPreset.OnValueChanged
                 .Subscribe(index =>
                 {
-                    var quality = _graphicsPresetValues[index];
+                    var quality = _graphicQualityValues[index];
                     Debug.Log($"Option Graphics: {index} => {quality}");
                 })
                 .AddTo(Disposables);
@@ -232,35 +312,108 @@ namespace Game.Horror.Dialogs
 
             #region Audio
 
-            _masterVolume.OnValueChanged
-                .Subscribe(value =>
-                {
-                    Debug.Log($"Option Master Volume: {value}");
-                })
-                .AddTo(Disposables);
-
-            _bgmVolume.OnValueChanged
-                .Subscribe(value =>
-                {
-                    Debug.Log($"Option BGM Volume: {value}");
-                })
-                .AddTo(Disposables);
-
-            _voiceVolume.OnValueChanged
-                .Subscribe(value =>
-                {
-                    Debug.Log($"Option Voice Volume: {value}");
-                })
-                .AddTo(Disposables);
-
-            _seVolume.OnValueChanged
-                .Subscribe(value =>
-                {
-                    Debug.Log($"Option SE Volume: {value}");
-                })
-                .AddTo(Disposables);
+            // _masterVolume.OnValueChanged
+            //     .Subscribe(value =>
+            //     {
+            //         Debug.Log($"Option Master Volume: {value}");
+            //     })
+            //     .AddTo(Disposables);
+            //
+            // _bgmVolume.OnValueChanged
+            //     .Subscribe(value =>
+            //     {
+            //         Debug.Log($"Option BGM Volume: {value}");
+            //     })
+            //     .AddTo(Disposables);
+            //
+            // _voiceVolume.OnValueChanged
+            //     .Subscribe(value =>
+            //     {
+            //         Debug.Log($"Option Voice Volume: {value}");
+            //     })
+            //     .AddTo(Disposables);
+            //
+            // _seVolume.OnValueChanged
+            //     .Subscribe(value =>
+            //     {
+            //         Debug.Log($"Option SE Volume: {value}");
+            //     })
+            //     .AddTo(Disposables);
 
             #endregion
+        }
+    }
+
+    public class HorrorOptionDialogModel : IDisposable
+    {
+        private bool _isDirty;
+
+        private string _localCode;
+
+        private float _masterVolume;
+        private float _bgmVolume;
+        private float _voiceVolume;
+        private float _seVolume;
+
+        public HorrorOptionDialogModel()
+        {
+            LoadSaveData();
+        }
+
+        private void LoadSaveData()
+        {
+        }
+
+        public void SetLocalCode(string localCode)
+        {
+            if (_localCode == localCode) return;
+            SetDirty();
+            _localCode = localCode;
+        }
+
+        public void SetMasterVolume(float volume)
+        {
+            if (_masterVolume.Equals(volume)) return;
+            SetDirty();
+            _masterVolume = volume;
+        }
+
+        public void SetBgmVolume(float volume)
+        {
+            if (_bgmVolume.Equals(volume)) return;
+            SetDirty();
+            _bgmVolume = volume;
+        }
+
+        public void SetVoiceVolume(float volume)
+        {
+            if (_voiceVolume.Equals(volume)) return;
+            SetDirty();
+            _voiceVolume = volume;
+        }
+
+        public void SetSeVolume(float volume)
+        {
+            if (_seVolume.Equals(volume)) return;
+            SetDirty();
+            _seVolume = volume;
+        }
+
+        public bool IsDirty() => _isDirty;
+
+        private void SetDirty() => _isDirty = true;
+
+        public void Reset()
+        {
+            _isDirty = false;
+        }
+
+        public void Dispose()
+        {
+            if (_isDirty)
+            {
+                // Save
+            }
         }
     }
 }
