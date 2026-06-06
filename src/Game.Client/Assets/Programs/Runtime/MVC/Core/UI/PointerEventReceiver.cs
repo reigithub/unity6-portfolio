7d4d0f722b86851aaed105305c54aee7a1b1d519
Rei.K <rei.engineer.hub@gmail.com>
2026-06-06 17:04:54 +0900

    同一フレーム内での再選択

--- src/Game.Client/Assets/Programs/Runtime/MVC/Core/UI/PointerEventReceiver.cs
+++ src/Game.Client/Assets/Programs/Runtime/MVC/Core/UI/PointerEventReceiver.cs
@@ -8,18 +8,30 @@ namespace Game.Core.UI
     public class PointerEventReceiver : MonoBehaviour
         , IPointerEnterHandler, IPointerExitHandler
         , IPointerClickHandler
+        , IPointerDownHandler, IPointerUpHandler
     {
         [SerializeField] private Selectable _selectable;
 
         private InputSystemService _inputService;
         private InputSystemService InputService => _inputService ??= GameServiceManager.Get<InputSystemService>();
 
-        public void OnPointerEnter(PointerEventData eventData) => OnSelected();
-        public void OnPointerExit(PointerEventData eventData) => OnDeselected();
-        public void OnPointerClick(PointerEventData eventData) => OnSelected();
+        public void OnPointerEnter(PointerEventData eventData) => OnSelect();
 
-        private void OnSelected() => InputService.SetSelectedGameObject(_selectable.gameObject);
-        private void OnDeselected() => InputService.SetSelectedGameObject(null);
+        public void OnPointerExit(PointerEventData eventData) { }
+
+        public void OnPointerClick(PointerEventData eventData) => OnPress(eventData);
+
+        public void OnPointerDown(PointerEventData eventData) => OnPress(eventData);
+
+        public void OnPointerUp(PointerEventData eventData) { }
+
+        private void OnPress(PointerEventData eventData)
+        {
+            if (eventData.button != PointerEventData.InputButton.Left) return;
+            OnSelect();
+        }
+
+        private void OnSelect() => InputService.SetSelectedGameObject(_selectable.gameObject);
 
 #if UNITY_EDITOR
         private void OnValidate()
