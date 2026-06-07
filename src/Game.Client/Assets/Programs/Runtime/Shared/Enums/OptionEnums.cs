56d37f67ff0dfab9faae3c489e8af0c62a1ead33
Rei.K <rei.engineer.hub@gmail.com>
2026-06-07 19:21:11 +0900

    enum FrameRateLimit削除

--- src/Game.Client/Assets/Programs/Runtime/Shared/Enums/OptionEnums.cs
+++ src/Game.Client/Assets/Programs/Runtime/Shared/Enums/OptionEnums.cs
@@ -1,14 +1,5 @@
 namespace Game.Shared.Enums
 {
-    public enum FrameRateLimit
-    {
-        Off = -1,
-        Limit30 = 30,
-        Limit60 = 60,
-        Limit90 = 90,
-        Limit120 = 120,
-    }
-
     public enum GraphicQuality
     {
         Custom = 0,
