20d14e437b6b910b60f0c79f12e60d9a2dfb14d0
Rei.K <rei.engineer.hub@gmail.com>
2026-06-08 20:40:12 +0900

    オプションセーブデータサービス初期化

--- src/Game.Client/Assets/Programs/Runtime/MVC/Horror/HorrorGameLauncher.cs
+++ src/Game.Client/Assets/Programs/Runtime/MVC/Horror/HorrorGameLauncher.cs
@@ -1,6 +1,7 @@
 using Cysharp.Threading.Tasks;
 using Game.Core;
 using Game.Core.Services;
+using Game.Horror.SaveData;
 using Game.Horror.Scenes;
 using Game.Shared.Bootstrap;
 using Game.Shared.Enums;
@@ -32,6 +33,12 @@ namespace Game.Horror
             var saveDataStorage = new SaveDataStorage();
             var audioSaveService = new AudioSaveService(saveDataStorage, audioService);
             await audioSaveService.LoadAsync();
+
+            // 4-2. オプション設定: ロード → 共有登録 → 起動時の静的適用
+            var optionSaveService = new HorrorOptionSaveService(saveDataStorage);
+            await optionSaveService.LoadAsync();
+            GameServiceManager.Register<HorrorOptionSaveService>(optionSaveService);
+            HorrorOptionHelper.ApplySaveData(optionSaveService.Data);
 
             // 5. 初期シーン遷移
             await gameSceneService.TransitionAsync<HorrorTitleScene>();
