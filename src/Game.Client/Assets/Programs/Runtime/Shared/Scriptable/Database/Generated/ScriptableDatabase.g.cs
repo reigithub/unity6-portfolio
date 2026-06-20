45f64e6b8688e04da805e0290ca8b9dcacfb44ed
Rei.K <rei.engineer.hub@gmail.com>
2026-06-21 00:46:07 +0900

    Build Database

--- src/Game.Client/Assets/Programs/Runtime/Shared/Scriptable/Database/Generated/ScriptableDatabase.g.cs
+++ src/Game.Client/Assets/Programs/Runtime/Shared/Scriptable/Database/Generated/ScriptableDatabase.g.cs
@@ -7,6 +7,9 @@ namespace Game.Shared.Scriptable.Database
     [CreateAssetMenu(menuName = "Scriptable Database/Database")]
     public sealed partial class ScriptableDatabase : ScriptableObject
     {
+        [SerializeField] private global::Game.Shared.Scriptable.Database.Tables.HorrorItemMasterTable horrorItemMasterTable;
+        public global::Game.Shared.Scriptable.Database.Tables.HorrorItemMasterTable HorrorItemMasterTable => horrorItemMasterTable;
+
         [SerializeField] private global::Game.Shared.Scriptable.Database.Tables.WeaponLevelMasterTable weaponLevelMasterTable;
         public global::Game.Shared.Scriptable.Database.Tables.WeaponLevelMasterTable WeaponLevelMasterTable => weaponLevelMasterTable;
     }
