60c62b25d376acebf21ea510b440f7545d32e2d2
Rei.K <rei.engineer.hub@gmail.com>
2026-06-22 09:07:01 +0900

    HorrorInteractionMaster

--- src/Game.Client/Assets/Programs/Runtime/Shared/Scriptable/Database/Generated/WeaponLevelMasterTable.g.cs
+++ src/Game.Client/Assets/Programs/Runtime/Shared/Scriptable/Database/Generated/WeaponLevelMasterTable.g.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using Game.Shared.Scriptable.Database;
-using Game.Shared.Scriptable.Database.Tables;
 
 namespace Game.Shared.Scriptable.Database.Tables
 {
