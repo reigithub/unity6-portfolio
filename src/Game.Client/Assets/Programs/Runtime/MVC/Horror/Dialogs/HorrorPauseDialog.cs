7e0b7c8cfe1746ff56f0f78d21333db19d066379
Rei.K <rei.engineer.hub@gmail.com>
2026-06-26 09:10:23 +0900

    オプション画面コンポーネント分割

---
+++ src/Game.Client/Assets/Programs/Runtime/MVC/Horror/Dialogs/HorrorPauseDialog.cs
@@ -0,0 +1,89 @@
+using Cysharp.Threading.Tasks;
+using Game.Core.Services;
+using Game.MVC.Core.Enums;
+using Game.MVC.Core.Scenes;
+using Game.Shared.Bootstrap;
+using Game.Shared.Extensions;
+using R3;
+
+namespace Game.Horror.Dialogs
+{
+    public enum PauseResult
+    {
+        Resume,
+        ReturnToTitle,
+        Quit
+    }
+
+    public class HorrorPauseDialog : GameDialogScene<HorrorPauseDialog, HorrorPauseDialogComponent, PauseResult>
+    {
+        protected override string AssetPathOrAddress => "HorrorPauseDialog";
+
+        private InputSystemService _inputService;
+
+        public static async UniTask<PauseResult> RunAsync()
+        {
+            PauseResult result;
+            var inputService = GameServiceManager.Get<InputSystemService>();
+            using (inputService.BlockPlayer())
+            using (inputService.BlockInputActions(inputService.UI.Inventory))
+            {
+                var sceneService = GameServiceManager.Get<GameSceneService>();
+                result = await sceneService.TransitionDialogAsync<HorrorPauseDialog, PauseResult>();
+            }
+            return result;
+        }
+
+        public override UniTask PreInitialize()
+        {
+            _inputService = GameServiceManager.Get<InputSystemService>();
+            ApplicationEvents.PauseTime();
+            return base.PreInitialize();
+        }
+
+        public override UniTask Startup()
+        {
+            Observable.Merge(_inputService.UI.Cancel.OnPerformedAsObservable(), _inputService.UI.Menu.OnPerformedAsObservable())
+                .Where(_ => State.IsProcessing())
+                .Subscribe(_ => TrySetResult(default))
+                .AddTo(Disposables);
+
+            SceneComponent.OnResume
+                .Subscribe(_ =>
+                {
+                    SceneComponent.SetInteractable(false);
+                    TrySetResult(PauseResult.Resume);
+                })
+                .AddTo(Disposables);
+            SceneComponent.OnOption
+                .SubscribeAwait(async (_, _) =>
+                {
+                    await HorrorOptionDialog.RunAsync();
+                })
+                .AddTo(Disposables);
+            SceneComponent.OnReturn
+                .Subscribe(_ =>
+                {
+                    SceneComponent.SetInteractable(false);
+                    TrySetResult(PauseResult.ReturnToTitle);
+                })
+                .AddTo(Disposables);
+            SceneComponent.OnQuit
+                .Subscribe(_ =>
+                {
+                    SceneComponent.SetInteractable(false);
+                    TrySetResult(PauseResult.Quit);
+                })
+                .AddTo(Disposables);
+
+            return base.Startup();
+        }
+
+        public override UniTask Terminate()
+        {
+            ApplicationEvents.ResumeTime();
+
+            return base.Terminate();
+        }
+    }
+}
