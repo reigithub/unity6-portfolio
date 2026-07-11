b45df6dd8ebac53b3033b326110dc87a80b681c3
Rei.K <rei.engineer.hub@gmail.com>
2026-07-11 23:11:21 +0900

    InteractionsLocalizer

--- src/Game.Client/Assets/Programs/Runtime/Shared/Constants/LocalizationConstants.cs
+++ src/Game.Client/Assets/Programs/Runtime/Shared/Constants/LocalizationConstants.cs
@@ -5,5 +5,6 @@ namespace Game.Shared.Constants
         public const string InputControlsTable = "InputControls";
         public const string ContextActionsTable = "ContextActions";
         public const string InteractionMessagesTable = "InteractionMessages";
+        public const string InteractionsTable = "Interactions";
     }
 }
