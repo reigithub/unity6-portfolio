2d97d3ff291e0a58744b01e3d549eef3a6a64fbd
Rei.K <rei.engineer.hub@gmail.com>
2026-07-23 04:38:15 +0900

    rename: MessagesTable

--- src/Game.Client/Assets/Programs/Runtime/Shared/Constants/LocalizationConstants.cs
+++ src/Game.Client/Assets/Programs/Runtime/Shared/Constants/LocalizationConstants.cs
@@ -4,7 +4,7 @@ namespace Game.Shared.Constants
     {
         public const string InputControlsTable = "InputControls";
         public const string ContextActionsTable = "ContextActions";
-        public const string InteractionMessagesTable = "InteractionMessages";
+        public const string MessagesTable = "Messages";
         public const string InteractionsTable = "Interactions";
         public const string PropTextsTable = "PropTexts";
         public const string UITextsTable = "UITexts";
